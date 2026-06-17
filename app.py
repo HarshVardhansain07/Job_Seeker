0eab045f57b32e5d2fc68fd0cac928e139d1e930
Harshvardhan <harshvardhan0007s@gmail.com>
2026-06-17 19:31:04 +0530

    added login and registeration page

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect,flash
 from datetime import datetime
 import sys
-
-# allow import from parent folder
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import database
 import Analysischart
-
-
+from flask import Flask, session
+from dotenv import load_dotenv
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt()
+# allow import from parent folder
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(
@@ -17,28 +17,79 @@ app = Flask(
     template_folder=os.path.join(BASE_DIR,"Frontend", "templates"),
     static_folder=os.path.join(BASE_DIR, "Frontend","static")
 )
+load_dotenv()
+app.secret_key = os.getenv("SECURITY_KEY")
 
 # create table
+database.User_table()
 database.create_table()
 # ---------------- HOME ----------------
-@app.route("/")
-def home():
-    data = database.view_data()
-    return render_template("home.html", data=data)
+@app.route("/",methods= ["GET","POST"])
+def login():
+    if request.method == "POST":
+     
+        Username= request.form['Username']
+        Password= request.form['Password']
+        User_Id = database.login(Username,Password)
+        if User_Id:
+            session["user_id"] = User_Id
+            return redirect("/Home")
+        
+    
+    return render_template("Login_form.html")
 
+
+@app.route("/Registration_form", methods=["GET", "POST"])
+def Registration():
+
+    if request.method == "POST":
+
+        result = database.Registration(
+            request.form['Username'],
+            request.form['Email'],
+            request.form['Password']
+        )
+
+        if result == "User already exists":
+
+            return render_template(
+                "Registration_form.html",
+                error="Username or Email already exists"
+            )
+
+        return redirect("/")
+
+    return render_template("Registration_form.html")
+    
+
+
+
+
+@app.route("/Home")
+
+def home():
+
+    if "user_id" not in session:
+
+        return redirect("/")
+
+    data = database.view_data(session["user_id"])
+
+    return render_template("home.html", data=data)
 
 # ---------------- ADD ----------------
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == 'POST':
         database.add_data(
+            session["user_id"],
             request.form['Company_Name'],
             request.form['HR_Email'],
             request.form['Role'],
             request.form['Status'],
             request.form['Website_link']
         )
-        return redirect("/")
+        return redirect("/Home")
     
     return render_template("add_form.html")
 
@@ -46,16 +97,20 @@ def add():
 # ---------------- UPDATE STATUS ----------------
 @app.route("/update", methods=["POST"])
 def update():
+
     Id = request.form.get("Id")
     status = request.form.get("status")
 
-    print(f"Updating Id: {Id}, Status: {status}")  # Debug
+    print(f"Updating Id: {Id}, Status: {status}")
 
     if Id and status:
-        database.update_status(status, Id)
+        database.update_status(
+            status,
+            Id,
+            session["user_id"]
+        )
 
-    return redirect("/")
-
+    return redirect("/Home")
 
 # ---------------- DELETE ----------------
 @app.route("/delete", methods=["POST"])
@@ -75,6 +130,13 @@ def analytics_page():
     stats = Analysischart.stats()
 
     return render_template("analytics.html", data=data, stats=stats)
+@app.route("/logout")
+
+def logout():
+
+    session.clear()
+
+    return redirect("/")
 
 
 # ---------------- RUN ----------------
--- database.py
+++ database.py
@@ -2,6 +2,9 @@ import mysql.connector
 from datetime import datetime
 from dotenv import load_dotenv
 import os
+from flask_bcrypt import Bcrypt
+from flask import flash
+bcrypt = Bcrypt()
 load_dotenv()
 
 def get_connection():
@@ -25,26 +28,38 @@ def create_table():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS `List` (
             Id INT AUTO_INCREMENT PRIMARY KEY,
+            User_Id INT NOT NULL,
             Company_Name VARCHAR(255) NOT NULL,
             HR_Email VARCHAR(255) NOT NULL,
             Date_applied DATE NOT NULL,
             Status VARCHAR(100) NOT NULL,
             Role VARCHAR(255) NOT NULL,
-            Website_link VARCHAR(500) NOT NULL
+            Website_link VARCHAR(500) NOT NULL,
+            
+            FOREIGN KEY (User_Id)
+                REFERENCES USERS(Id)
+                ON DELETE CASCADE
         )
     """)
 
     conn.commit()
-    print("Table creation query executed")
     cursor.close()
     conn.close()
 
 
-def view_data():
+def view_data(User_Id):
     conn = get_connection()
     cursor = conn.cursor()
 
-    cursor.execute("SELECT * FROM `List`")
+    cursor.execute("""
+
+        SELECT *
+
+        FROM List
+
+        WHERE User_Id = %s
+
+    """, (User_Id,))
     data = cursor.fetchall()
 
     cursor.close()
@@ -53,7 +68,7 @@ def view_data():
     return data
 
 
-def add_data(Company_Name, HR_Email, Role, Status, Website_link, Date_applied=None):
+def add_data(User_Id,Company_Name, HR_Email, Role, Status, Website_link, Date_applied=None):
     conn = get_connection()
     cursor = conn.cursor()
 
@@ -63,10 +78,23 @@ def add_data(Company_Name, HR_Email, Role, Status, Website_link, Date_applied=No
     cursor.execute(
         """
         INSERT INTO `List`
-        (Company_Name, HR_Email, Role, Status, Website_link, Date_applied)
-        VALUES (%s, %s, %s, %s, %s, %s)
+        (User_ID,
+        Company_Name,
+        HR_Email,
+        Role,
+        Status,
+        Website_link,
+        Date_applied
+        )
+        VALUES (%s,%s, %s, %s, %s, %s, %s)
         """,
-        (Company_Name, HR_Email, Role, Status, Website_link, Date_applied)
+        (User_Id,
+        Company_Name,
+        HR_Email,
+        Role,
+        Status,
+        Website_link,
+        Date_applied)
     )
 
     conn.commit()
@@ -74,13 +102,13 @@ def add_data(Company_Name, HR_Email, Role, Status, Website_link, Date_applied=No
     conn.close()
 
 
-def update_status(Status, Id):
+def update_status(Status, Id,User_Id):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute(
-        "UPDATE `List` SET Status = %s WHERE Id = %s",
-        (Status, Id)
+        "UPDATE `List` SET Status = %s WHERE Id = %s AND User_ID = %s",
+        (Status, Id,User_Id)
     )
 
     conn.commit()
@@ -88,15 +116,108 @@ def update_status(Status, Id):
     conn.close()
 
 
-def delete_data(Id):
+def delete_data(Id,User_Id):
     conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute(
-        "DELETE FROM `List` WHERE Id = %s",
-        (Id,)
+        "DELETE FROM List WHERE Id = %s AND User_Id = %s",
+        (Id,User_Id)
+    )
+
+    conn.commit()
+    cursor.close()
+    conn.close()   
+# --------------*--------*--------*--------*--------*--------*--------*--------*--------*--------*--------* 
+# User Table 
+
+def User_table():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS `USERS` (
+            Id INT AUTO_INCREMENT PRIMARY KEY,
+            USERNAME VARCHAR(255) UNIQUE NOT NULL ,
+            Mail VARCHAR(255)  UNIQUE NOT NULL,
+            PASSWORD VARCHAR(500) NOT NULL
+                    
+        )
+    """)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    # Registration || Signup
+
+def Registration(Username ,Mail, Password):     
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+
+        """
+        SELECT Id
+
+        FROM USERS
+
+        WHERE USERNAME = %s OR Mail = %s
+        """,
+        (Username, Mail)
+    )
+    existing_user = cursor.fetchone()
+
+    if existing_user:
+
+        cursor.close()
+
+        conn.close()
+
+        return "User already exists"
+    if not Username or not Mail or not Password:
+        cursor.close()
+        conn.close()
+        return "All fields are required"
+    
+    Passwords = bcrypt.generate_password_hash(Password).decode('utf-8')
+    cursor.execute(
+        """
+        INSERT INTO `USERS`
+        (Username,
+        Mail,
+        Password)
+        VALUES (%s, %s, %s)
+        """,
+        (Username,
+        Mail,
+        Passwords)
     )
 
     conn.commit()
     cursor.close()
     conn.close()
+    return "Success"
+
+#  login 
+def login(Username, Password):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT Id,PASSWORD FROM USERS WHERE USERNAME = %s",
+        (Username,)
+    )
+
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    if result is None:
+        return None
+    User_Id = result[0]
+    stored_hash = result[1]
+    
+    if bcrypt.check_password_hash(stored_hash, Password):
+        return User_Id
+    return None
+
